ec894454ae5bf1a16e6d559858ed30aa4d2f81c9
mattpascoe <hornet136@gmail.com>
2015-10-26 13:49:45 -0600

    update version number and document link

--- plugin_info.php
+++ plugin_info.php
@@ -4,8 +4,8 @@ Your plugin must have a short description and a version number
 */
 
 $plugin_description = 'Build Bind DNS server configurations.';
-$plugin_version = '1.0';
-$plugin_help_url = 'http://opennetadmin.com/docs/plugins/build_bind';
+$plugin_version = '1.1';
+$plugin_help_url = 'https://github.com/opennetadmin/'.$plug_name.'/wiki'';
 
 
 ?>
