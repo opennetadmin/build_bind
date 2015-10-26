5457bf2243cb630e009a74511e3f6ce417f426d7
mattpascoe <hornet136@gmail.com>
2015-10-26 14:12:52 -0600

    plug_name! ugh

--- plugin_info.php
+++ plugin_info.php
@@ -2,6 +2,7 @@
 /*
 Your plugin must have a short description and a version number
 */
+$plug_name = basename(dirname(__FILE__));
 
 $plugin_description = 'Build Bind DNS server configurations.';
 $plugin_version = '1.1';
