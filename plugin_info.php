ef0601e15a24fe6adab0363322e580cddf1baa6a
Matt Pascoe <matt@opennetadmin.com>
2015-11-10 04:26:04 +0000

    Fixing issue #12.. ugh stupid typos

--- plugin_info.php
+++ plugin_info.php
@@ -6,7 +6,7 @@ $plug_name = basename(dirname(__FILE__));
 
 $plugin_description = 'Build Bind DNS server configurations.';
 $plugin_version = '1.1';
-$plugin_help_url = 'https://github.com/opennetadmin/'.$plug_name.'/wiki'';
+$plugin_help_url = 'https://github.com/opennetadmin/'.$plug_name.'/wiki';
 
 
 ?>
